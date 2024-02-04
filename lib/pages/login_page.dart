6f63a4c746b03d67e804abf3fed6be5d738f1c93
MasRadeen <sigitnugrohop@gmail.com>
2024-02-04 10:15:20 +0800

    finalisasi file login_page, menambahkan icon social media pada assets icons

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -18,80 +18,134 @@ class LoginPage extends StatelessWidget {
             ),
           ),
           //layer body
-          Column(
-            children: [
-              SizedBox(
-                height: 135,
-              ),
-              Container(
-                // color: Colors.amber,
-                // width: MediaQuery.of(context).size.width,
-                width: 200,
-                height: 200,
-                child: Image.asset(
-                  "assets/images/pic-1.png",
-                  fit: BoxFit.contain,
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 35),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 100,
                 ),
-              ),
-              Text("Welcome to "),
-              Text("Dirbbox"),
-              Container(
-                width: 250,
-                child: Text(
-                  "Best cloud storage platform for all business and individuals to manage there data\n\n Join For Free.",
-                )
-              ),
-              Row(
-                children: [
-                  ElevatedButton(
-                    onPressed: () {}, 
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Image.asset("assets/icons/finger.png"),
-                        SizedBox(width: 10,),
-                        Text(
-                          "Smart ID",
-                          style: TextStyle(
-                            color: Color(0xFF567DF4),
+                Container(
+                  // color: Colors.amber,
+                  // width: MediaQuery.of(context).size.width,
+                  width: 200,
+                  height: 200,
+                  child: Image.asset(
+                    "assets/images/pic-1.png",
+                    fit: BoxFit.contain,
+                  ),
+                ),
+                SizedBox(height: 20,),
+                Text(
+                  "Welcome to ",
+                  style: TextStyle(
+                    fontSize: 22,
+                  ),
+                ),
+                Text(
+                  "Dirbbox",
+                  style: TextStyle(
+                    fontSize: 35,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Container(
+                  width: 250,
+                  child: Text(
+                    "Best cloud storage platform for all business and individuals to manage there data\n\nJoin For Free.",
+                    style: TextStyle(
+                      fontSize: 16,
+                    ),
+                  )
+                ),
+                SizedBox(height: 20,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {}, 
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset("assets/icons/finger.png"),
+                            SizedBox(width: 10,),
+                            Text(
+                              "Smart ID",
+                              style: TextStyle(
+                                color: Color(0xFF567DF4),
+                              ),
+                            ),
+                          ],
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          primary: Color(0xFFEEF7FE),
+                          fixedSize: Size(150, 50),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15),
                           ),
                         ),
-                      ],
-                    ),
-                    style: ElevatedButton.styleFrom(
-                      primary: Color(0xFF567DF4).withOpacity(0.4),
-                      padding: EdgeInsets.symmetric(
-                        vertical: 10,
-                        horizontal: 20,
                       ),
                     ),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {}, 
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "Sign In",
-                          style: TextStyle(
-                            color: Colors.white,
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {}, 
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "Sign In",
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                            SizedBox(width: 10,),
+                            Image.asset("assets/icons/panah-kanan.png"),
+                          ],
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          primary: Color(0xFF567DF4),
+                          fixedSize: Size(150, 50),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15),
                           ),
                         ),
-                        SizedBox(width: 10,),
-                        Image.asset("assets/icons/panah-kanan.png"),
-                      ],
-                    ),
-                    style: ElevatedButton.styleFrom(
-                      primary: Color(0xFF567DF4).withOpacity(0.4),
-                      padding: EdgeInsets.symmetric(
-                        vertical: 10,
-                        horizontal: 20,
                       ),
                     ),
+                  ],
+                ),
+                SizedBox(height: 20,),
+                Center(
+                  child: Text(
+                    "Use Social Login",
+                    style: TextStyle(
+                      fontSize: 16,
+                    ),
                   ),
-                ],
-              ),
-            ],
+                ),
+                SizedBox(height: 10,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset("assets/icons/ig.png"),
+                    SizedBox(width: 60,),
+                    Image.asset("assets/icons/x.png"),
+                    SizedBox(width: 60,),
+                    Image.asset("assets/icons/fb.png"),
+                  ],
+                ),
+                SizedBox(height: 10,),
+                Center(
+                  child: Text(
+                    "Create an Account",
+                    style: TextStyle(
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
